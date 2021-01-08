3a654292b2daf215b204b06d5c977ecae44186d4
EklavyaLamba <lambaeklavya@gmail.com>
2021-01-08 12:12:35 +0530

    Modified Pig.js

--- Pig.js
+++ Pig.js
@@ -2,6 +2,27 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
+  
+    this.visible=255;
+  }
+   display(){
+   console.log(this.body.speed)
+   if(this.body.speed>4){
+    push()
+    this.visible=this.visible-5;
+     tint(255,this.visible)
+    image(this.image,this.body.position.x,this.body.position.y,this.width,this.height) 
+    pop()
+    World.remove(world,this.body)
+
+   }
+   else  { super.display()
   }
 
-};
+   
+  
+  
+  }
+
+
+   }     
